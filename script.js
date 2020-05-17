0da3d33705d4863bff6d5328d272b36ce4f77a9d
DMCatanach <dawn.catanach@gmail.com>
2020-05-16 22:58:20 -0600

    Add function to compare player guesses.

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ function generateTarget() {
 	targetNum = Math.floor(Math.random() * 10);
 	return targetNum;
 }
+
+function compareGuesses(humanGuess, computerGuess, targetNum) {
+	if (humanGuess === computerGuess) {
+		return true; 
+	}
+	// comparison of absolute values of difference between guesses and target 
+	if (Math.abs(targetNum - humanGuess) < Math.abs(targetNum - computerGuess)) {
+		return true; 
+	} else {
+		return false; 
+	}
+}
